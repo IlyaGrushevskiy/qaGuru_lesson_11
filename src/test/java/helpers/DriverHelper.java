767f3de00c003d0b1e54445c6fe7e90f695e00c5
IlyaGrushevskiy <belpochta05@gmail.com>
2021-04-04 10:25:28 +0300

    add telegramBot

--- src/test/java/helpers/DriverHelper.java
+++ src/test/java/helpers/DriverHelper.java
@@ -1,8 +1,14 @@
 package helpers;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import config.ConfigHelper;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
 
 public class DriverHelper {
     public static void configureDriver() {
@@ -17,5 +23,13 @@ public class DriverHelper {
             Configuration.browserCapabilities = capabilities;
             Configuration.remote = ConfigHelper.getWebRemoteDriver();
         }
+    }
+
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString().replace("selenoid","");
+    }
+
+    public static String getConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
     }
 }
